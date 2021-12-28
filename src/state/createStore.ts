1706cc9197b30be6a88d690cd2c9a9e19c3f7f25
Lee Savage <leesavage09@gmail.com>
2021-12-28 18:43:54 +0200

    added .env

--- src/state/createStore.ts
+++ src/state/createStore.ts
@@ -6,6 +6,6 @@ const reducer = combineReducers({
   recipes: recipeReducer
 })
 
-const createStore =  () => reduxCreateStore(reducer, devToolsEnhancer({}));//TODO devtoos togel with env
+const createStore =  () => reduxCreateStore(reducer, devToolsEnhancer({}));
 
 export default createStore
