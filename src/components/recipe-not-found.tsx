eb542959cd6bde5bca7603e1a1a7bbc25cf67d56
Lee Savage <leesavage09@gmail.com>
2021-12-28 18:22:02 +0200

    suported sites made into links

--- src/components/recipe-not-found.tsx
+++ src/components/recipe-not-found.tsx
@@ -1,54 +1,60 @@
 import * as React from "react"
 import Layout from "./layout"
 
-const RecipeNotFound = () => (//TODO make links
+const suportedSites = [
+    "https://www.101cookbooks.com/",
+    "https://www.allrecipes.com/",
+    "https://www.ambitiouskitchen.com/",
+    "https://www.averiecooks.com/",
+    "https://www.bbc.co.uk/",
+    "https://www.bbcgoodfood.com/",
+    "https://www.bonappetit.com/",
+    "https://www.budgetbytes.com/",
+    "https://www.centraltexasfoodbank.org/",
+    "https://www.closetcooking.com/",
+    "https://cookieandkate.com/",
+    "https://copykat.com/",
+    "https://damndelicious.net/",
+    "https://www.eatingwell.com/",
+    "https://www.epicurious.com/",
+    "https://www.food.com/",
+    "https://www.foodandwine.com/",
+    "https://www.foodnetwork.com/",
+    "https://gimmedelicious.com/",
+    "https://www.gimmesomeoven.com/",
+    "https://julieblanner.com/",
+    "https://www.kitchenstories.com/",
+    "https://www.melskitchencafe.com/",
+    "https://www.minimalistbaker.com/",
+    "https://www.myrecipes.com/",
+    "https://www.nomnompaleo.com/",
+    "https://www.omnivorescookbook.com/",
+    "https://pinchofyum.com/",
+    "https://recipetineats.com/",
+    "https://www.seriouseats.com/",
+    "https://www.simplyrecipes.com/",
+    "https://smittenkitchen.com/",
+    "https://thepioneerwoman.com/",
+    "https://www.tasteofhome.com/",
+    "https://tastesbetterfromscratch.com/",
+    "https://thatlowcarblife.com/",
+    "https://www.theblackpeppercorn.com/",
+    "https://therealfoodrds.com/",
+    "https://www.thespruceeats.com/",
+    "https://whatsgabycooking.com/",
+    "https://www.woolworths.com.au/",
+    "https://www.yummly.com/",
+    "https://www.jamieoliver.com/"
+]
+
+const RecipeNotFound = () => (
     <Layout title="Recipe Not Found" show_back_btn={true}>
         <p>The URL you entered could not be imported, Unfortunately not all cooking websites are currently supported.</p>
         <p>The following are supported recipe websites</p>
         <ul>
-            <li>https://www.101cookbooks.com/</li>
-            <li>https://www.allrecipes.com/</li>
-            <li>https://www.ambitiouskitchen.com/</li>
-            <li>https://www.averiecooks.com/</li>
-            <li>https://www.bbc.co.uk/</li>
-            <li>https://www.bbcgoodfood.com/</li>
-            <li>https://www.bonappetit.com/</li>
-            <li>https://www.budgetbytes.com/</li>
-            <li>https://www.centraltexasfoodbank.org/</li>
-            <li>https://www.closetcooking.com/</li>
-            <li>https://cookieandkate.com/</li>
-            <li>https://copykat.com/</li>
-            <li>https://damndelicious.net/</li>
-            <li>https://www.eatingwell.com/</li>
-            <li>https://www.epicurious.com/</li>
-            <li>https://www.food.com/</li>
-            <li>https://www.foodandwine.com/</li>
-            <li>https://www.foodnetwork.com/</li>
-            <li>https://gimmedelicious.com/</li>
-            <li>https://www.gimmesomeoven.com/</li>
-            <li>https://julieblanner.com/</li>
-            <li>https://www.kitchenstories.com/</li>
-            <li>https://www.melskitchencafe.com/</li>
-            <li>https://www.minimalistbaker.com/</li>
-            <li>https://www.myrecipes.com/</li>
-            <li>https://www.nomnompaleo.com/</li>
-            <li>https://www.omnivorescookbook.com/</li>
-            <li>https://pinchofyum.com/</li>
-            <li>https://recipetineats.com/</li>
-            <li>https://www.seriouseats.com/</li>
-            <li>https://www.simplyrecipes.com/</li>
-            <li>https://smittenkitchen.com/</li>
-            <li>https://thepioneerwoman.com/</li>
-            <li>https://www.tasteofhome.com/</li>
-            <li>https://tastesbetterfromscratch.com/</li>
-            <li>https://thatlowcarblife.com/</li>
-            <li>https://www.theblackpeppercorn.com/</li>
-            <li>https://therealfoodrds.com/</li>
-            <li>https://www.thespruceeats.com/</li>
-            <li>https://whatsgabycooking.com/</li>
-            <li>https://www.woolworths.com.au/</li>
-            <li>https://www.yummly.com/</li>
-            <li>https://www.jamieoliver.com/</li>
+            {suportedSites.map((site, idx) => {
+                return <li key={idx}><a href={site}>{site}</a></li>
+            })}
         </ul>
     </Layout>
 )
