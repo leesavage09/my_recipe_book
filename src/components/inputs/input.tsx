77e7876a2ebeb608473b97f4e3283161ee43932b
Lee Savage <leesavage09@gmail.com>
2021-12-28 14:58:24 +0200

    textarea onchange bug fix

--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { ChangeEventHandler } from 'react'
 import style from './scss/input.module.scss'
 
-const Input = ({ rows, type, placeholder, className, onChange, value }:{ rows?:number, type?:String, placeholder:string, className?:String, onChange:Function, value:string }) => {
+const Input = ({ rows, type, placeholder, className, onChange, value }:{ rows?:number, type?:String, placeholder:string, className?:String, onChange:ChangeEventHandler, value:string }) => {
 //TODO There should be to text areas
     if (type === "textarea") {
         return (
-            <textarea rows={rows} placeholder={placeholder} className={`${style.text_input} ${className}`} onChange={()=>onChange()} value={value} />
+            <textarea rows={rows} placeholder={placeholder} className={`${style.text_input} ${className}`} onChange={onChange} value={value} />
         )
     }
     else {
         return (
-            <input placeholder={placeholder} className={`${style.text_input} ${className}`} onChange={()=>onChange()} value={value} />
+            <input placeholder={placeholder} className={`${style.text_input} ${className}`} onChange={onChange} value={value} />
         )
     }
 }
