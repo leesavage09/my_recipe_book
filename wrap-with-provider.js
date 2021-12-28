3eafa81be4252ae079d84d9dd20008a37bdf9434
Lee Savage <leesavage09@gmail.com>
2021-12-28 19:11:33 +0200

    added .env

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,16 +1,14 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import createStore from "./src/state/createStore"
 
 const WrapWithProvider = ({ element }) => {
 
   const store = createStore()
 
-  //https://my-recipe-book-server.herokuapp.com/graphql
-  //http://localhost:3001/graphql
   const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
+    uri: process.env.GATSBY_APOLLO_CLIENT_URI,
     cache: new InMemoryCache()
   });
 
